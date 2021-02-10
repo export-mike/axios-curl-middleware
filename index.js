755b51836db859efef8bf2969f09587d9776c3df
Tom W <tom.w@med.app>
2021-02-10 14:41:56 +1100

    Writing to file system correctly with post body

--- index.js
+++ index.js
@@ -1,70 +1,98 @@
-const curl = require('curl-cmd');
-const {parse, resolve} = require('url');
+const curlMaker = require('curl-cmd');
+const { parse } = require('url');
+const urljoin = require('url-join');
+const uuid = require('uuid/v4');
 
-const normaliseKeys = (obj) => {
-    return Object.entries(obj).reduce((acc, [key, value]) => {
-        return {
-            ...acc,
-            [key.toLowerCase()]: typeof(value) === 'object' ? normaliseKeys(value) : value 
-        }
-    }, {})
+const normaliseKeys = obj => {
+  return Object.entries(obj).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [key.toLowerCase()]:
+        typeof value === 'object' ? normaliseKeys(value) : value,
+    };
+  }, {});
+};
+
+function isObject(data) {
+  return typeof data === 'object';
 }
 
-function axiosCurl(axios, {
-    log = (str) => { console.log(str) },
-    writeToFS = false
-} = {}) {
-    axios.interceptors.request.use(function (config) {
-        const url = config.baseURL ? resolve(config.baseURL, config.url) : config.url;
-        const {hostname, port, path} = parse(url)
-        const configHeaders = normaliseKeys(config.headers);
-        const {
-            common, delete: del, get, head, post, put, patch,
-            ...restHeaders
-        } = configHeaders;
-        const methodHeaders = {
-            get, head, post, put, patch,
-        };
-        methodHeaders['delete'] = del;
-        const headers = {
-            ...common,
-            ...methodHeaders[config.method],
-            ...restHeaders
-        } 
-        const isJSON = headers['content-type'] === 'application/json';
-        const json = isJSON ? `--data ${JSON.stringify(config.data)}` : '';
-        log(`${curl.cmd({
-            hostname,
-            port,
-            path: path,
-            method: config.method,
-            headers,
-        })} ${json}`);
-        if (writeToFS) {
-            const fs = require('fs');
-            const kebabCase = require('lodash.kebabcase');
-            const process = require('process');
-            const util = require('util');
-            const { join: joinPath } = require('path');
-            const promisify = util.promisify(fs.writeFile);
-            const writeFile = promisify;
-            fs.mkdir(joinPath(__dirname, 'curls'),
-                { recursive: true}, (err) => {
-                    if (err) {
-                        return console.error(err);
-                    }
-                }
-            )
-            writeFile(joinPath(process.cwd(), `./src/curls/${kebabCase(path)}.curl.txt`), curl.cmd({
-                hostname,
-                port,
-                path,
-                method: config.method,
-                headers,
-            }), 'utf-8').catch(e => console.error(e));
+function axiosCurl(
+  axios,
+  {
+    log = str => {
+      console.log(str);
+    },
+    writeToFS = true,
+  } = {}
+) {
+  // eslint-disable-next-line func-names
+  axios.interceptors.request.use(async function (config) {
+    const url = config.baseURL
+      ? urljoin(config.baseURL, config.url)
+      : config.url;
+    const { hostname, path, protocol } = parse(url);
+    console.log('config', config);
+    const configHeaders = normaliseKeys(config.headers);
+    const {
+      common,
+      delete: del,
+      get,
+      head,
+      post,
+      put,
+      patch,
+      ...restHeaders
+    } = configHeaders;
+    const methodHeaders = {
+      get,
+      head,
+      post,
+      put,
+      patch,
+    };
+    methodHeaders.delete = del;
+    const headers = {
+      ...common,
+      ...methodHeaders[config.method],
+      ...restHeaders,
+    };
+    const isJSON =
+      headers['content-type'] === 'application/json' || isObject(config.data);
+    const json = isJSON ? `--data '${JSON.stringify(config.data)}'` : '';
+
+    const href = `${protocol}//${hostname}${path}`;
+    const curl = `${curlMaker.cmd({
+      href,
+      method: config.method.toUpperCase(),
+      headers,
+    })} ${json}`;
+    log(curl);
+    if (writeToFS) {
+      const fs = require('fs');
+      const kebabCase = require('lodash.kebabcase');
+      const process = require('process');
+      const util = require('util');
+      const { join: joinPath } = require('path');
+      const promisify = util.promisify(fs.writeFile);
+      const writeFile = promisify;
+      fs.mkdir(joinPath(process.cwd(), 'curls'), { recursive: true }, err => {
+        if (err) {
+          return console.error('>>>', err);
         }
-        return config;
-    });
+      });
+      await writeFile(
+        joinPath(
+          process.cwd(),
+          'curls',
+          `${kebabCase(path)}.curl.${uuid()}.txt`
+        ),
+        curl,
+        'utf-8'
+      ).catch(e => console.error(e));
+    }
+    return config;
+  });
 }
 
 module.exports = axiosCurl;
